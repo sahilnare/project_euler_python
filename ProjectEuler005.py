3bbfa6ec13f8410f778f73a1ff44348e1848bdf5
Sahil Nare <37807526+sahilnare@users.noreply.github.com>
2019-03-04 03:54:21 +0530

    Add files via upload

---
+++ ProjectEuler005.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 02:19:53 2019
+
+@author: Sahil
+"""
+
+
+
+
+def find_gcd(a,b):
+    gcd = 1
+    for i in range(2,min(a,b)):
+        while a%i==0 and b%i==0:
+            a = a/i
+            b = b/i
+            gcd = gcd*i
+    return gcd
+
+ans = 1
+
+for k in range(1,11):
+    ans = int((ans*k)/(find_gcd(ans,k)))
+
+print(ans)
