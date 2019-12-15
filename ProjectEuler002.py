2dd5c00ee2e653d932173b44e8952eccb071c9a4
Sahil Nare <37807526+sahilnare@users.noreply.github.com>
2019-12-15 21:46:57 +0530

    Update ProjectEuler002.py

--- ProjectEuler002.py
+++ ProjectEuler002.py
@@ -5,6 +5,7 @@ Created on Sun Mar  3 21:52:07 2019
 @author: Sahil
 """
 
+
 fibo = [0,1]
 i = 2
 sumo = 0
@@ -14,5 +15,6 @@ while((fibo[i-1] + fibo[i-2]) < 4000000):
     if fibo[i]%2 == 0:
         sumo += fibo[i]
     i+=1
+
 
 print(sumo)
